915f7a1ada9242b412a2857ec53d51bf4f2199a5
vimala-siravi <vimala1995@gmail.com>
2023-08-29 10:52:12 +0530

    created app dor custom button component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@vimala-siravi/my-library";
+import { useState } from "react";
 
 function App() {
+  const [showText, setShowText] = useState(false);
+
+  const toggleText = () => {
+    setShowText((prevState) => !prevState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto px-4 py-4">
+      <h1 className="ml-5 text-center font-bold">Button component</h1>
+      <Button variant="outlined" onClick={() => toggleText()}>
+        Hello World
+      </Button>
+
+      {showText && <div className="mt-4">Hi there !</div>}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Button component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Button component/i);
   expect(linkElement).toBeInTheDocument();
 });
